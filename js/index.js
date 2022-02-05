353a62491873b9403bbc00cbe3e33ca8af384bcb
Areola Daniel <66014556+lifeofdaniell@users.noreply.github.com>
2022-02-05 21:57:36 +0100

    add smooth scroll

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ import LocomotiveScroll from 'locomotive-scroll';
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
+    tablet: { smooth: true },
+    smartphone: { smooth: true },
     multiplier: .5,
     lerp: 0.05
 });
