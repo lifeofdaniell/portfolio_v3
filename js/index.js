d92a598c8e544d608554615430f003f68fc36bdc
Areola Daniel <66014556+lifeofdaniell@users.noreply.github.com>
2022-02-05 21:36:41 +0100

    init

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
-const scroll = new LocomotiveScroll();
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    multiplier: .5,
+    lerp: 0.05
+});
