72e9cf9f3c373a9052156bb9d2499dfd9bc74c45
Areola Daniel <66014556+lifeofdaniell@users.noreply.github.com>
2022-02-05 23:41:33 +0100

    fix smooth scroll

--- js/index.js
+++ js/index.js
@@ -4,11 +4,6 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
     tablet: { smooth: true },
-    smartphone: {
-        smooth: true,
-        multiplier: 2,
-        lerp: 0.1,
-    },
     multiplier: 0.65,
-    lerp: 0.05
+    lerp: 0.075
 });
