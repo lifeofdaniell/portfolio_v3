a80f8287629afc9604531f5a59b30e8f6dda9dea
Areola Daniel <66014556+lifeofdaniell@users.noreply.github.com>
2022-02-08 05:56:51 +0100

    fit smooth scroll bug

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
-import LocomotiveScroll from 'locomotive-scroll'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    tablet: {smooth: true},
-    multiplier: 0.65,
-    lerp: 0.075,
-  })
 
   
 
--- js/transition.js
+++ js/transition.js
@@ -1,5 +1,4 @@
-import barba from '@barba/core';
-// import gsap from 'gsap'
+import barba from '@barba/core'
 import {
   homeReveal,
   homeToPage,
@@ -8,230 +7,190 @@ import {
   aboutReveal,
   aboutToPage,
   projectReveal,
-  projectToPage
-} from "./animations";
+  projectToPage,
+} from './animations'
 import LocomotiveScroll from 'locomotive-scroll'
 
-let scroll
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+  tablet: {
+    smooth: true,
+    breakpoint: 991
+  },
+  lerp: 0.075,
+  reloadOnContextChange: true
+})
 
-barba.hooks.after(() => {
-	scroll.update();
-});
+barba.hooks.beforeLeave((data) => {
+  scroll.destroy()
+})
+
+barba.hooks.after((data) => {
+  scroll.init()
+})
 
 barba.init({
   transitions: [
     //Page Loaded Animations
     {
       name: 'Home Reveal',
-      to: { namespace: 'home' },
-      once ({ next }) {
+      to: {namespace: 'home'},
+      once({next}) {
         homeReveal()
-        initLocoScroll()
       },
-      enter ({ next }) {
+      enter({next}) {
         homeReveal()
       },
     },
     {
       name: 'Skill Reveal',
-      to: { namespace: ['web', 'photography', 'mobile'] },
-      once ({ next }) {
+      to: {namespace: ['web', 'photography', 'mobile']},
+      once({next}) {
         skillReveal()
-        initLocoScroll()
       },
     },
     {
       name: 'About Reveal',
-      to: { namespace: ['about'] },
-      once ({ next }) {
-          aboutReveal()
-          initLocoScroll()
+      to: {namespace: ['about']},
+      once({next}) {
+        aboutReveal()
       },
     },
     {
       name: 'Project Reveal',
-      to: { namespace: ['btm', 'events', 'portraits', 'stills'] },
-      once ({ next }) {
-          projectReveal()
-          initLocoScroll()
+      to: {namespace: ['btm', 'events', 'portraits', 'stills']},
+      once({next}) {
+        projectReveal()
       },
     },
+
     //Page Transition Animations
     {
       name: 'Home to Skill',
-      from: { namespace: 'home' },
-      to: { namespace: ['web', 'photography', 'mobile'] },
+      from: {namespace: 'home'},
+      to: {namespace: ['web', 'photography', 'mobile']},
       leave: (data) => homeToPage(data.current),
-      enter ({ next }) {
-        scroll.update();
+      enter({next}) {
         skillReveal()
-      }
+      },
     },
     {
       name: 'Skill to Home',
-      from: { namespace: ['web', 'photography', 'mobile'] },
-      to: { namespace: 'home' },
+      from: {namespace: ['web', 'photography', 'mobile']},
+      to: {namespace: 'home'},
       leave: (data) => skillToPage(data.current),
-      enter ({ next }) {
+      enter({next}) {
         homeReveal()
-      }
+      },
     },
     {
       name: 'Home to About',
-      from: { namespace: 'home' },
-      to: { namespace: 'about' },
+      from: {namespace: 'home'},
+      to: {namespace: 'about'},
       leave: (data) => homeToPage(data.current),
-      enter ({ next }) {
-        scroll.update();
+      enter({next}) {
         aboutReveal()
-      }
+      },
     },
     {
       name: 'About to Home',
-      from: { namespace: 'about' },
-      to: { namespace: 'home' },
+      from: {namespace: 'about'},
+      to: {namespace: 'home'},
       leave: (data) => aboutToPage(data.current),
-      enter ({ next }) {
+      enter({next}) {
         homeReveal()
-      }
+      },
     },
     {
       name: 'About to Skill',
-      from: { namespace: 'about' },
-      to: { namespace: ['web', 'photography', 'mobile']  },
+      from: {namespace: 'about'},
+      to: {namespace: ['web', 'photography', 'mobile']},
       leave: (data) => aboutToPage(data.current),
-      enter ({ next }) {
-        scroll.update();
-         skillReveal()
-      }
+      enter({next}) {
+        skillReveal()
+      },
     },
     {
       name: 'Skill to About',
-      from: { namespace: ['web', 'photography', 'mobile']  },
-      to: { namespace: 'about' },
+      from: {namespace: ['web', 'photography', 'mobile']},
+      to: {namespace: 'about'},
       leave: (data) => skillToPage(data.current),
-      enter ({ next }) {
-        scroll.update();
-         aboutReveal()
-      }
+      enter({next}) {
+        aboutReveal()
+      },
     },
     {
       name: 'Skill to Skill',
-      from: { namespace: ['web', 'photography', 'mobile']  },
-      to: { namespace: ['web', 'photography', 'mobile']  },
+      from: {namespace: ['web', 'photography', 'mobile']},
+      to: {namespace: ['web', 'photography', 'mobile']},
       leave: (data) => skillToPage(data.current),
-      enter ({ next }) {
-        scroll.update();
-         skillReveal()
-      }
+      enter({next}) {
+        skillReveal()
+      },
     },
     {
       name: 'Skill to Project',
-      from: { namespace:  'photography' },
-      to: { namespace: ['btm', 'events', 'portraits', 'stills'] },
+      from: {namespace: 'photography'},
+      to: {namespace: ['btm', 'events', 'portraits', 'stills']},
       leave: (data) => skillToPage(data.current),
-      enter ({ next }) {
-        scroll.update()
-        initLocoScroll()
+      enter({next}) {
         projectReveal()
-        // initLocoScroll()
       },
-      beforeEnter() {
-				scroll.setScroll(0,0);
-			}
     },
     {
       name: 'Project to Project',
-      from: { namespace: ['btm', 'events', 'portraits', 'stills'] },
-      to: { namespace: ['btm', 'events', 'portraits', 'stills'] },
+      from: {namespace: ['btm', 'events', 'portraits', 'stills']},
+      to: {namespace: ['btm', 'events', 'portraits', 'stills']},
       leave: (data) => projectToPage(data.current),
-      enter ({ next }) {
-        scroll.update();
-        initLocoScroll()
+      enter({next}) {
         projectReveal()
-        // initLocoScroll()
       },
-      beforeEnter() {
-				scroll.setScroll(0,0);
-			}
     },
     {
       name: 'Project to Home',
-      from: { namespace: ['btm', 'events', 'portraits', 'stills'] },
-      to: { namespace: 'home' },
+      from: {namespace: ['btm', 'events', 'portraits', 'stills']},
+      to: {namespace: 'home'},
       leave: (data) => projectToPage(data.current),
-      enter ({ next }) {
+      enter({next}) {
         homeReveal()
       },
     },
     {
       name: 'Project to About',
-      from: { namespace: ['btm', 'events', 'portraits', 'stills'] },
-      to: { namespace: 'about' },
+      from: {namespace: ['btm', 'events', 'portraits', 'stills']},
+      to: {namespace: 'about'},
       leave: (data) => projectToPage(data.current),
-      enter ({ next }) {
+      enter({next}) {
         aboutReveal()
-        // initLocoScroll()
       },
-      beforeEnter() {
-				scroll.setScroll(0,0);
-			}
     },
     {
       name: 'Project to Skill',
-      from: { namespace: ['btm', 'events', 'portraits', 'stills'] },
-      to: { namespace:  'photography' },
+      from: {namespace: ['btm', 'events', 'portraits', 'stills']},
+      to: {namespace: 'photography'},
       leave: (data) => projectToPage(data.current),
-      enter ({ next }) {
+      enter({next}) {
         skillReveal()
       },
     },
     {
       name: 'Home to Project',
-      from: { namespace: 'home' },
-      to: { namespace:['btm', 'events', 'portraits', 'stills']  },
+      from: {namespace: 'home'},
+      to: {namespace: ['btm', 'events', 'portraits', 'stills']},
       leave: (data) => homeToPage(data.current),
-      enter ({ next }) {
-        scroll.update();
-        initLocoScroll()
+      enter({next}) {
         projectReveal()
-      }
+      },
     },
     {
       name: 'About to Project',
-      from: { namespace: 'about' },
-      to: { namespace:['btm', 'events', 'portraits', 'stills']  },
+      from: {namespace: 'about'},
+      to: {namespace: ['btm', 'events', 'portraits', 'stills']},
       leave: (data) => aboutToPage(data.current),
-      enter ({ next }) {
-        scroll.update();
-        initLocoScroll()
+      enter({next}) {
         projectReveal()
       },
-       beforeEnter() {
-				scroll.setScroll(0,0);
-			}
     },
-  ]
-});
-
-
- function initLocoScroll() {
-	scroll = new LocomotiveScroll({
-		el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    tablet: {smooth: true},
-    multiplier: 0.65,
-    lerp: 0.075,
-  });
- }
-
-   function isMobile(x) {
-    if (x.matches) {
-    
-    } else {
-      
-    }
-  }
-  var x = window.matchMedia('(max-width: 767px)')
-  isMobile(x) // Call listener function at run time
-  x.addListener(isMobile) // Attach listener function on state changes
+  ],
+})
